36c680fdb899d3d6af3498a1acd52013fb6ca0fd
MiaWangGettinJob <113963408+MiaWangGettinJob@users.noreply.github.com>
2022-10-16 11:21:29 -0700

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,6 +1,6 @@
-class problem1:
+class Problem1(object):
 
-    def rotation(nums, k):
+    def rotation(self, nums, k):
         length = len(nums)
         def helper(nums):
             start = 0
@@ -9,15 +9,23 @@ class problem1:
                 nums[start], nums[end] = nums[end], nums[start]
                 start += 1
                 end -= 1
+            return nums
 
         helper(nums)
         helper(nums[:k])
         helper(nums[k:])
 
-        return nums
+        return helper(nums[:k])+helper(nums[k:])
 
 
+def main():
+    nums = [1,2,3,4,5,6,7]
+    k = 3
+    problem1 = Problem1()
+    print(problem1.rotation(nums, k))
 
+
+main()
 
 
 
